fd0588dd195d8b0e6bd8f831afb75fd61a31c612
Eric Mailman <emailman@dtcc.edu>
2018-10-02 13:59:42 -0400

    First version

---
+++ OOP-1/Account1.cs
@@ -0,0 +1,45 @@
+﻿namespace OOP_1
+{
+    class Account1
+    {
+        // Instance variables
+        private readonly string name;
+        private decimal balance;
+
+        public Account1(string acctName, decimal acctBalance = 0)
+        {
+            name = acctName;
+            balance = acctBalance;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        private void SetBalance(decimal amt)
+        {
+            balance = amt;
+        }
+
+        public void Deposit(decimal amt)
+        {
+            SetBalance(GetBalance() + amt);
+        }
+
+        public void Withdraw(decimal amt)
+        {
+            SetBalance(GetBalance() - amt);
+        }
+
+        override public string ToString()
+        {
+            return $"Account: {name} / Balance = {balance:C}";
+        }
+    }
+}
--- OOP-1/Program.cs
+++ OOP-1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOP_1
 {
@@ -10,6 +6,43 @@ namespace OOP_1
     {
         static void Main(string[] args)
         {
+            // Create some accounts in an array
+            Account1[] accounts = {
+                                    new Account1("Dopey", 500),
+                                    new Account1("Sleepy"),
+                                    new Account1("Sneezy", 300)
+                                   };
+
+            Console.WriteLine("Opening Report");
+            Console.WriteLine("--------------");
+            Reports(accounts);
+
+            // Do some transactions
+            accounts[0].Withdraw(150);
+            accounts[1].Deposit(100);
+            accounts[2].Deposit(75);
+
+            Console.WriteLine("Closing Report");
+            Console.WriteLine("--------------");
+            Reports(accounts);
+        }
+
+        static void Reports(Account1[] accts)
+        {
+            Console.WriteLine("List of accounts and balances:");
+            foreach (var acct in accts)
+                Console.WriteLine(acct);
+            Console.WriteLine();
+
+            Console.WriteLine("List of account names:");
+            foreach (var acct in accts)
+                Console.WriteLine(acct.GetName());
+
+            Console.Write("\nTotal Balance = ");
+            decimal balance = 0;
+            foreach (var acct in accts)
+                balance += acct.GetBalance();
+            Console.WriteLine($"{balance:C}\n");
         }
     }
 }
